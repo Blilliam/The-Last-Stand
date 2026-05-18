d45da053e5f4bce3c25bec41867df5abb0ab0302
Blilliam <167144267+Blilliam@users.noreply.github.com>
2026-05-18 08:37:56 -0400

    Add files via upload

---
+++ src/Open/Artifacts/Common/Clover.java
@@ -0,0 +1,19 @@
+package Open.Artifacts.Common;
+
+import Open.Artifacts.Artifact;
+import main.Assets;
+import main.GameObject;
+
+public class Clover extends Artifact { // still not implemented
+    public Clover(GameObject gameObj) {
+        super(gameObj);
+        this.name = "Clover";
+        this.icon = Assets.CommonCloverIcon;
+        desc = "Increases luck by 15%";
+    }
+
+    @Override
+    public double getPercentLuck() {
+        return 0.10 * count;
+    }
+}
