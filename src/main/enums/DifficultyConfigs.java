633b3c6ae3e155a071016117a680ec86d8df33ec
Blilliam <167144267+Blilliam@users.noreply.github.com>
2026-06-08 08:15:00 -0400

    Delete src directory

--- src/main/enums/DifficultyConfigs.java
+++
@@ -1,7 +0,0 @@
-package main.enums;
-
-public class DifficultyConfigs {
-	public static final DifficultyConfig EASY = new DifficultyConfig(0.20, 0.02, 0.75, 1.0, 60);
-	public static final DifficultyConfig NORMAL = new DifficultyConfig(0.30, 0.03, 1.00, 1.5, 80);
-	public static final DifficultyConfig HARD = new DifficultyConfig(0.45, 0.05, 1.40, 2.0, 100);
-}
